f9411276f20d6e371c8950d17729f0a568c4a91c
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-26 16:02:41 -0800

    Created Login UIScreen

---
+++ app/components/Input.js
@@ -0,0 +1,15 @@
+import React, { Component }  from 'react';
+import { StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import { SimpleLineIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+//FINISH. Need to learn redux
+export default class Input extends Component {
+
+
+  render(){
+
+    const { } = this.props
+
+    
+  }
+}
--- app/screens/login.js
+++ app/screens/login.js
@@ -1,7 +1,8 @@
 import React, { Component }  from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
-import { Input, Button, Icon } from 'react-native-elements';
+import { StyleSheet, Text, View, TouchableHighlight, TextInput, Button, TouchableOpacity } from 'react-native';
+import {  Icon } from 'react-native-elements';
 import  {LinearGradient}  from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class Login extends React.Component { 
 
@@ -22,25 +23,44 @@ export default class Login extends React.Component {
       end={{x:1.0, y: 1.0}}
       locations={[0.1,0.8]}
       >
-        <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeHolderText='Enter your username'
-            value={this.state.username}
-            onChangeText={(text) => this.setState({username: text})}
-          />
 
-          <TextInput 
-            style={styles.input}
-            placeHolderText='Enter your password'
-            secureTextEntry={true}
-            value={this.state.password}
-            onChangeText={(text) => this.setState({password:text})}
-          />
-          <Button
-            style={styles.button}
-            title='Login'
-          />
+      <Text style={styles.title}> anml. </Text> 
+        <View style={styles.form}>
+
+       
+
+        <View style={styles.inputContainer}>
+          <Ionicons 
+            name="md-person" 
+            size={24} 
+            color='#FBFAE1'
+            style={styles.icon}
+            />  
+           <TextInput
+              style={styles.input}
+              value={this.state.username}
+              onChangeText={(text) => this.setState({username: text})}
+            />         
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Ionicons 
+            name="md-lock" 
+            size={24} 
+            color='#FBFAE1'
+            style={styles.icon}
+            />  
+           <TextInput
+              style={styles.input}
+              value={this.state.password}
+              onChangeText={(text) => this.setState({password: text})}
+            />
+        </View>         
+        <TouchableOpacity
+          style={styles.button}
+        >
+        <Text style={styles.buttonText}> Login </Text> 
+        </TouchableOpacity>
         </View>
       </LinearGradient> 
     );
@@ -51,18 +71,60 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 
   input: {
-    height: 60,
     width: 250,
-    padding: 10,
-    borderBottomWidth: 1,
-    borderColor: '#FBFAE1'
+    paddingBottom: 5,
+    paddingLeft: 10,
+    fontFamily: 'Futura',
+    fontSize: 16,
+    color: '#053A2E',
+    fontWeight: 'bold'
   },
 
   button: {
     padding: 30,
+    width: 160,
+    height: 40,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: '#18EBBB',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    borderBottomWidth: 1,
+    borderColor: '#F1EFB9',
+    marginBottom: 40,
+  },
+
+  form: {
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+  },
+
+  icon: {
+    paddingLeft: 5,
+  },
+
+  buttonText: {
+    fontFamily: 'Futura',
+    color: '#fff',
+    fontSize: 24
+  },
+
+  title: {
+    paddingTop: 10,
+    fontFamily: 'Futura', 
+    fontSize: 36,
+    color: '#F1EFB9',
+    marginTop: 100,
   }
 });
