d4b6712dec75402c1f4566fb4c6277394dfcd980
Ryanliszewski <ryanliszewski@gmail.com>
2018-03-31 01:06:33 -0700

    edit profile page started

--- app/screens/EditProfile.js
+++ app/screens/EditProfile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {  View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableHighlight } from 'react-native';
+import { Input } from 'react-native-elements';
 
 export default class componentName extends Component {
   
@@ -17,15 +18,52 @@ export default class componentName extends Component {
   //Profile Picture
   //Banner Picture 
 
+  _renderProfileImage = () => {
+
+    const { profile_image } = this.props.user.user
+
+    if(this.props.user.user.profile_image){
+      return(
+        <Image
+          source={{uri: profile_image}}
+          style ={{
+            width: 100,
+            height: 100, 
+            borderRadius: 50,
+            borderWidth: 2, 
+            borderColor: '#F1EFB9'
+          }}
+        />
+      )
+    }
+  }
+
+  handleUpdateProfilePicture = () => {
+
+  }
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.container}>
+        {this._renderProfileImage()}
+        <TouchableHighlight
+          onPress={() => this.handleUpdateProfilePicture}
+        >
+        <Text>Update Profile Picture</Text>
+
+        <Input/>
+        <Input/>
+
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
